d40485e71468aabc663dee2e515cc562bb99d449
Heidycpp <31572400+Heidycpp@users.noreply.github.com>
2017-12-04 22:37:56 -0500

    initial

--- Bamazon.sql
+++ Bamazon.sql
@@ -1,16 +1,5 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               5.7.12-log - MySQL Community Server (GPL)
--- Server OS:                    Win64
--- HeidiSQL Version:             9.3.0.4984
--- --------------------------------------------------------
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Dumping structure for table bamazon.departments
 CREATE TABLE IF NOT EXISTS `departments` (
   `DepartmentID` int(11) NOT NULL AUTO_INCREMENT,
   `DepartmentName` varchar(100) NOT NULL,
@@ -19,18 +8,16 @@ CREATE TABLE IF NOT EXISTS `departments` (
   PRIMARY KEY (`DepartmentID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
--- Dumping data for table bamazon.departments: ~4 rows (approximately)
-/*!40000 ALTER TABLE `departments` DISABLE KEYS */;
+
 INSERT INTO `departments` (`DepartmentID`, `DepartmentName`, `OverHeadCosts`, `TotalSales`) VALUES
 	(1, 'Instruments', 1500, 15125.50),
 	(2, 'School Supplies', 1000, 1330.03),
 	(3, 'Attire', 2300, 1854.87),
 	(4, 'Household', 4000, 234.76),
 	(5, 'Electronics', 1700, 74500.50);
-/*!40000 ALTER TABLE `departments` ENABLE KEYS */;
 
 
--- Dumping structure for table bamazon.products
+
 CREATE TABLE IF NOT EXISTS `products` (
   `itemID` int(11) NOT NULL AUTO_INCREMENT,
   `ProductName` varchar(100) NOT NULL,
@@ -40,8 +27,7 @@ CREATE TABLE IF NOT EXISTS `products` (
   PRIMARY KEY (`itemID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
--- Dumping data for table bamazon.products: ~10 rows (approximately)
-/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+
 INSERT INTO `products` (`itemID`, `ProductName`, `DepartmentName`, `Price`, `StockQuantity`) VALUES
 	(1, 'Piano', 'Instruments', 999.99, 10),
 	(2, 'Guitar', 'Instruments', 57.34, 111),
@@ -57,7 +43,3 @@ INSERT INTO `products` (`itemID`, `ProductName`, `DepartmentName`, `Price`, `Sto
 	(12, 'Lamp', 'Household', 10.74, 19),
 	(13, 'Eraser', 'School Supplies', 0.10, 874),
 	(14, 'Headphones', 'Electronics', 48.50, 167);
-/*!40000 ALTER TABLE `products` ENABLE KEYS */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- BamazonExecutive.js
+++ BamazonExecutive.js
@@ -3,8 +3,7 @@ var inquirer = require('inquirer');
 var colors = require('colors');
 var Table = require('cli-table');
 
-var keys = require('./keys.js');		//Contains the host, port, user, password, and database to be used for in the mySQL database
-
+var keys = require('./keys.js');		
 var connection = mysql.createConnection(keys.connection);
 
 connection.connect(function(err) {
